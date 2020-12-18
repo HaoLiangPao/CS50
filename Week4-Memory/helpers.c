ff2d29aa2274dad970def7585475131b05d10acf
HaoLiangPao <hflsforeverhao@gmail.com>
2020-12-17 21:05:09 -0500

    GreyScale finished

--- Week4-Memory/helpers.c
+++ Week4-Memory/helpers.c
@@ -1,9 +1,26 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop through every row in the picture
+    for (int row = 0; row < height; row ++)
+    {
+        // Loop through every column to get every pixel
+        for (int column = 0; column < width; column ++)
+        {
+            int red = image[row][column].rgbtRed;
+            int green = image[row][column].rgbtGreen;
+            int blue = image[row][column].rgbtBlue;
+            float average = round(((float) red + green + blue) / 3);
+            image[row][column].rgbtBlue = (int) average;
+            image[row][column].rgbtRed = (int) average;
+            image[row][column].rgbtGreen = (int) average;
+        }
+    }
     return;
+
 }
 
 // Reflect image horizontally
