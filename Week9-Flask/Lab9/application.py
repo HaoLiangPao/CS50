6dd407e04c88d20a071c41b0638a40066e78bea9
HaoLiangPao <hflsforeverhao@gmail.com>
2021-01-25 14:37:48 -0500

    lab9 finished, new feature added: delete a record

--- Week9-Flask/Lab9/application.py
+++ Week9-Flask/Lab9/application.py
@@ -14,16 +14,33 @@ db = SQL("sqlite:///birthdays.db")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    #POST
     if request.method == "POST":
-
-        # TODO: Add the user's entry into the database
-
+        # A new delete feature
+        delete = request.form.get("delete")
+        if delete:
+            # Get the user input infomation
+            deleteName = request.form.get("deleteName")
+            deleteMonth = request.form.get("deleteMonth")
+            deleteDay = request.form.get("deleteDay")
+            # remove the specified record from the database
+            db.execute("DELETE FROM birthdays WHERE name=? AND month=? AND day=?", deleteName, deleteMonth, deleteDay)
+        else:
+            # Get the user input infomation
+            name = request.form.get("name")
+            month = request.form.get("month")
+            day = request.form.get("day")
+            if name != '' and month and day:
+                # Add the user's entry into the database
+                db.execute("INSERT INTO birthdays (name, month, day) VALUES (?,?,?)", name, month, day)
+        # Redirect to homepage to see newly made changes
         return redirect("/")
-
+    #GET
     else:
 
-        # TODO: Display the entries in the database on index.html
+        # Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays");
 
-        return render_template("index.html")
+        return render_template("index.html", birthdays = birthdays);
 
 
