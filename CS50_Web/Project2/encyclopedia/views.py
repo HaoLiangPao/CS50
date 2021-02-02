ed29a8ca2b60ac79337a0bbab19141f4257edf74
HaoLiangPao <hflsforeverhao@gmail.com>
2021-02-02 15:04:24 -0500

    Add a link to entry page, be able to get markdown content from an entry

---
+++ CS50_Web/Project2/encyclopedia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html",{
+        "entry": util.get_entry(title)
+    })
+
