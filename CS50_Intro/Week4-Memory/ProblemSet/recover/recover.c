0612de35c50f6fe894fb8205458a9a74cf7af21f
HaoLiangPao <hflsforeverhao@gmail.com>
2021-02-25 14:37:04 -0500

    recover.c finished, a long way went through, sprintf, fread, better understanding of memory allocates and manipulation

--- CS50_Intro/Week4-Memory/ProblemSet/recover/recover.c
+++ CS50_Intro/Week4-Memory/ProblemSet/recover/recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -22,42 +24,43 @@ int main(int argc, char *argv[])
     int number = 0;
     uint8_t buffer[512];
     bool foundImage = false;
-    string filename;
+    // char *filename = malloc(8 * sizeof(char)); // 123.jpg
+    char filename[8];
+    FILE *output = NULL;
 
     // There are 512 bytes to be read
-    while (fread(buffer, 512, 1, input) != 1) {
+    while (fread(buffer, 512, 1, input) == 1) {
         // Check for signitures
         if (buffer[0] == 0xff &&
             buffer[1] == 0xd8 &&
             buffer[2] == 0xff &&
             // Make sure the first four bites are 1110
-            (buffer[3] & 0xf0 == 0xe0)) {
+            (buffer[3] & 0xf0) == 0xe0) {
                 // If found a new image
                 if (foundImage == false) {
                     // Switch found flag
                     foundImage = true;
-                    // Found a new picture, assign a new name
-                    sprint(filename, "%3i.jpg", number);
-                    // Increase the numbering system for images
-                    number ++;
-                    FILE *output = fopen(filename, "w");
-                    // Write 512 bytes from the buffer into the new image file
-                    fwrite(output, 512, 1, buffer);
                 } else {
                     // Already found a new image
-                    // Switch found flag
-
                     fclose(output);
                 }
+                // free(filename);
+                // Found a new picture, assign a new name
+                sprintf(filename, "%3i.jpg", number);
+                // Increase the numbering system for images
+                printf("%s image is been created.\n", filename);
+                number ++;
+                output = fopen(filename, "w");
+                // Write 512 bytes from the buffer into the new image file
+                fwrite(buffer, 512, 1, output);
 
         }
         // Keep looking for more information of the current image
-        if ( buffer[0] != 0xff ||
+        if (foundImage && ( buffer[0] != 0xff ||
              buffer[1] != 0xd8 ||
              buffer[2] != 0xff ||
-             (buffer[3] & 0xf0 != 0xe0)) {
-                fwrite(output, 512, 1, buffer);
-                fread(buffer, 512, 1, input);
+             (buffer[3] & 0xf0) != 0xe0)) {
+                 fwrite(buffer, 512, 1, output);
         }
     }
 }
