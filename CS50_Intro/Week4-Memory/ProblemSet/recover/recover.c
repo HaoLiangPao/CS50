0cd4b089bc86ba9415af9b436f69617f42ca0063
HaoLiangPao <hflsforeverhao@gmail.com>
2021-02-25 15:34:00 -0500

    Style changed for recover, and file name changed for generated images

--- CS50_Intro/Week4-Memory/ProblemSet/recover/recover.c
+++ CS50_Intro/Week4-Memory/ProblemSet/recover/recover.c
@@ -29,38 +29,44 @@ int main(int argc, char *argv[])
     FILE *output = NULL;
 
     // There are 512 bytes to be read
-    while (fread(buffer, 512, 1, input) == 1) {
+    while (fread(buffer, 512, 1, input) == 1)
+    {
         // Check for signitures
         if (buffer[0] == 0xff &&
             buffer[1] == 0xd8 &&
             buffer[2] == 0xff &&
             // Make sure the first four bites are 1110
-            (buffer[3] & 0xf0) == 0xe0) {
-                // If found a new image
-                if (foundImage == false) {
-                    // Switch found flag
-                    foundImage = true;
-                } else {
-                    // Already found a new image
-                    fclose(output);
-                }
-                // free(filename);
-                // Found a new picture, assign a new name
-                sprintf(filename, "%3i.jpg", number);
-                // Increase the numbering system for images
-                printf("%s image is been created.\n", filename);
-                number ++;
-                output = fopen(filename, "w");
-                // Write 512 bytes from the buffer into the new image file
-                fwrite(buffer, 512, 1, output);
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            // If found a new image
+            if (foundImage == false)
+            {
+                // Switch found flag
+                foundImage = true;
+            }
+            else
+            {
+                // Already found a new image
+                fclose(output);
+            }
+            // free(filename);
+            // Found a new picture, assign a new name
+            sprintf(filename, "%.3i.jpg", number);
+            // Increase the numbering system for images
+            // printf("%s image is been created.\n", filename);
+            number ++;
+            output = fopen(filename, "w");
+            // Write 512 bytes from the buffer into the new image file
+            fwrite(buffer, 512, 1, output);
 
         }
         // Keep looking for more information of the current image
-        if (foundImage && ( buffer[0] != 0xff ||
-             buffer[1] != 0xd8 ||
-             buffer[2] != 0xff ||
-             (buffer[3] & 0xf0) != 0xe0)) {
-                 fwrite(buffer, 512, 1, output);
+        if (foundImage && (buffer[0] != 0xff ||
+                           buffer[1] != 0xd8 ||
+                           buffer[2] != 0xff ||
+                           (buffer[3] & 0xf0) != 0xe0))
+        {
+            fwrite(buffer, 512, 1, output);
         }
     }
 }
