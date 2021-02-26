f6126b9c19cc5bfa08b3e24758e5cb5990e11684
HaoLiangPao <hflsforeverhao@gmail.com>
2021-02-26 14:37:58 -0500

    DNA check finished

--- CS50_Intro/Week6-Python/ProblemSet/dna/dna.py
+++ CS50_Intro/Week6-Python/ProblemSet/dna/dna.py
@@ -14,23 +14,48 @@ def load_data(database):
         reader = csv.DictReader(f)
         for row in reader:
             people[row["name"]] = {
-                "AGATC": row["AGATC"],
-                "AATG": row["AATG"],
-                "TATC": row["TATC"]
+                "AGATC": int(row["AGATC"]),
+                "AATG": int(row["AATG"]),
+                "TATC": int(row["TATC"])
             }
 
 def main():
+    # Flag for compare result
+    found = False
+
     if len(sys.argv) > 3 or len(sys.argv) < 2:
         sys.exit("Usage: python dna.py [database.csv] [testing.txt]")
     database = sys.argv[1] if len(sys.argv) == 2 else "databases/small.csv"
     testing = sys.argv[2]
 
     # Load data from files into memory
-    print("Loading data...")
+    # print("Loading database...")
     load_data(database)
-    print("Data loaded.")
+    # print("Database loaded.")
 
-    print(people)
+    # Load DNA sequence from testing file
+    f = open(testing, "r")
+    sequence = f.readline()
+    # Searching for STRs
+    STRs = ["AGATC", "AATG", "TATC"]
+    counts = [0, 0, 0]
+    for index in range(len(sequence)):
+        if sequence[index : index + 5] == STRs[0]:
+            counts[0] += 1
+        elif sequence[index : index + 4] == STRs[1]:
+            counts[1] += 1
+        elif sequence[index : index + 4] == STRs[2]:
+            counts[2] += 1
+    # Comparing with the database records
+    for person in people.keys():
+        record = people[person]
+        if ((record["AGATC"] == counts[0]) and
+         (record["AATG"] == counts[1]) and
+         (record["TATC"] == counts[2])):
+           found = True
+           print(person)
+    if not found:
+        print("No match")
 
 
 if __name__ == "__main__":
