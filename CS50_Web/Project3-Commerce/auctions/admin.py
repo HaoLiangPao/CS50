4153d79ecae49a0ca8c4a14c4e1d20fc64e7a8c9
HaoLiangPao <hflsforeverhao@gmail.com>
2021-03-26 13:50:58 -0400

    admin added

--- CS50_Web/Project3-Commerce/auctions/admin.py
+++ CS50_Web/Project3-Commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Auction_Category)
