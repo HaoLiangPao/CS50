87302ad7ca25da93b9a4875db07d1848905a95a3
HaoLiangPao <hflsforeverhao@gmail.com>
2021-04-05 21:35:50 -0400

    Bind archive event to email detail page

--- CS50_Web/Project4-Mail/mail/static/mail/inbox.js
+++ CS50_Web/Project4-Mail/mail/static/mail/inbox.js
@@ -61,10 +61,66 @@ function sent_email(event) {
     event.preventDefault()
 }
 
+toggle_archive = function(event, id) {
+    console.log(`id is ${id}`);
+    // Get current status of the email
+    fetch(`/emails/${id}`)
+        .then(response => response.json)
+        .then(email => {
+            console.log(email.archived)
+            // Change the archive status based on the current status
+            fetch(`/emails/${email.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    archived: !email.archived
+                })
+              })
+        })
+}
+
+email_detail = function(email) {
+    return function curried_func(e) {
+        console.log('This element has been clicked!')
+        // 1. Create a email detail element
+        let email_element = document.createElement('div');
+        // 2. Change the content of the email-container to the email just created
+        const {sender, recipients, subject, timestamp, body} = email
+        document.querySelector('#from_value').innerHTML = sender
+        document.querySelector('#to_value').innerHTML = recipients
+        document.querySelector('#subject_value').innerHTML = subject
+        document.querySelector('#timestamp_value').innerHTML = timestamp
+        document.querySelector('#email-content').innerHTML = body
+    
+        // 3. Change the button to archive/unarchive
+        archive_button = document.querySelector('#archive')
+        archive_button.addEventListener('click', toggle_archive.bind(event, email.id), false)
+        if (email.archived) {
+            archive_button.innerHTML = "Unarchive"
+        } else {
+            archive_button.innerHTML = "Archive"
+        }
+    
+        // 4. Show the email-container, unshow the mail box views
+        document.querySelector('#emails-view').style.display = 'none';
+        clear_emails();
+        document.querySelector('#compose-view').style.display = 'none';
+        document.querySelector('#email-container').style.display = 'block';
+    
+        // 5. Mark the email as read in the database
+        fetch(`/emails/${email.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                read: true
+            })
+          })
+    }
+}
+
 function load_mailbox(mailbox, message=null) {
-  
+    
   // 1. Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  clear_emails();
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-container').style.display = 'none';
 
@@ -127,7 +183,7 @@ function load_mailbox(mailbox, message=null) {
                 record.appendChild(timestamp)
                 record.classList.add('email-record')
                 // When a email record is been clicked
-                record.addEventListener('click', () => email_detail(email));
+                record.addEventListener('click', email_detail(email));
                 // Make up the record row
                 record_row.appendChild(record)
                 // Add the record into the email table
@@ -139,29 +195,26 @@ function load_mailbox(mailbox, message=null) {
 
 }
 
-function email_detail(email) {
-    console.log('This element has been clicked!')
-    // 1. Create a email detail element
-    let email_element = document.createElement('div');
-    // 2. Change the content of the email-container to the email just created
-    const {sender, recipients, subject, timestamp, body} = email
-    document.querySelector('#from_value').innerHTML = sender
-    document.querySelector('#to_value').innerHTML = recipients
-    document.querySelector('#subject_value').innerHTML = subject
-    document.querySelector('#timestamp_value').innerHTML = timestamp
-    document.querySelector('#email-content').innerHTML = body
+// @TODO: how to remove event lisenters?
+function clear_emails() {
+    // Apporoach 1: remove event listeners
+    let emails = document.querySelector('#emails')
+    console.log(emails);
+    // Hide emails container
+    emails.style.display = 'none';
+    // Remove event listeners
+    for (let child of emails.children) {
+        child.children[0].removeEventListener('click', email_detail);
+    }
+    // Wide out everything
+    emails.innerHTML = '';
 
-    // 3. Show the email-container, unshow the mail box views
-    document.querySelector('#emails-view').style.display = 'none';
-    document.querySelector('#emails').style.display = 'none';
-    document.querySelector('#compose-view').style.display = 'none';
-    document.querySelector('#email-container').style.display = 'block';
+    // // Approach 2: Create a new div, replace the old one
+    // let new_emails = document.createElement('ul')
+    // new_emails.id = "emails"
+    // document.querySelector('.container').replaceChild(new_emails, document.querySelector('#emails'))
 
-    // 4. Mark the email as read in the database
-    fetch('/emails/100', {
-        method: 'PUT',
-        body: JSON.stringify({
-            read: true
-        })
-      })
+    // Remove eventlisteners
+    document.querySelector('#archive').removeEventListener('click', toggle_archive)
+    document.querySelector('#reply').removeEventListener('click', toggle_archive)
 }
